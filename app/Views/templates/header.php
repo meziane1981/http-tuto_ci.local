eb2dc1109a670904266327549883bc59d80c5dd0
meziane1981 <meziane_maamar@yahoo.fr>
2022-12-07 18:01:09 +0100

    8  commit

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg bg-light">
+  <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">MEZIANE Maamar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
