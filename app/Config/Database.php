043c7cfd52f46075076b359867ef3553123a2e13
meziane1981 <meziane_maamar@yahoo.fr>
2022-12-12 17:46:54 +0100

    10  commit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => 'maamar',
-        'password' => '8LkgZ8H6',
-        'database' => 'devel',
+        'username' => '',
+        'password' => '',
+        'database' => '',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => true,
